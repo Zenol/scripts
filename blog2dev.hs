2bb498b7e01abcdf55a0ad86fb3f63829a5672aa
Jeremy Cochoy <jeremy.cochoy@gmail.com>
2013-07-29 19:08:37 +0200

    Add dvp scripts

--- blog2dev.hs
+++ blog2dev.hs
@@ -11,14 +11,21 @@ work :: [String] -> [String]
 work l = case l of
   "" : xs   -> "</paragraph>\n<paragraph>" : work xs
   x  : xs | "<pre lang=\"haskell\" colla=\"+\">" `isPrefixOf` x
-            -> "<code langage=\"haskell\">\n<!CDATA[\n"
+            -> "</paragraph>\n<code langage=\"haskell\">\n<![CDATA[\n"
                : work ((drop (length "<pre lang=\"haskell\" colla=\"+\">") $ x) : xs)
   x  : xs | "<pre lang=\"cpp\" colla=\"+\">" `isPrefixOf` x
-            -> "<code langage=\"cpp\">\n<!CDATA[\n"
+            -> "</paragraph>\n<code langage=\"cpp\">\n<![CDATA[\n"
                : work ((drop (length "<pre lang=\"cpp\" colla=\"+\">") $ x) : xs)
   x  : xs | "<h3>" `isPrefixOf` x && "</h3>" `isSuffixOf` x
             ->(
-              "<section id=\"??\">\n<title>"
+              "<section id=\"h3??\">\n<title>"
+              ++ (take (length x - 9) . drop 4 $ x)
+              ++ "</title>\n<paragraph>\n"
+              )
+              : work xs
+  x  : xs | "<h4>" `isPrefixOf` x && "</h4>" `isSuffixOf` x
+            ->(
+              "<section id=\"h4??\">\n<title>"
               ++ (take (length x - 9) . drop 4 $ x)
               ++ "</title>\n<paragraph>\n"
               )

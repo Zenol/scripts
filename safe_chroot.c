5cc42e8189ffef5ab55f6d0427c0814ee522ba67
Jeremy Cochoy <jeremy.cochoy@gmail.com>
2013-08-02 12:06:29 +0200

    \n is always a good idea

--- safe_chroot.c
+++ safe_chroot.c
@@ -8,25 +8,25 @@ int main(int argc, char* argv[])
     if (argc < 3)
     {
         //printf("%s NEWROOT COMMAND [ARG]", argv[0]);
-        printf("Failed to launch process...");
+        printf("Failed to launch process...\n");
         return 0;
     }
     if (chdir(argv[1]) < 0)
     {
-        printf("Failed to launch process...");
+        printf("Failed to launch process...\n");
         //perror("chdir failed");
         return 1;
     }
 
     if (chroot(argv[1]) < 0)
     {
-        printf("Failed to launch process...");
+        printf("Failed to launch process...\n");
         //perror("chroot failed");
         return 1;
     }
     if (setuid(42) < 0)
     {
-        printf("Failed to launch process...");
+        printf("Failed to launch process...\n");
         //perror("setuid failed");
         return 1;
     }

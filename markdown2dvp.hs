167eb2b9e5590f47924865985bd8591c40e3a5fa
Jeremy Cochoy <jeremy.cochoy@gmail.com>
2013-08-23 03:23:11 +0200

    Handle part of bloks in markdown2dvp.

--- markdown2dvp.hs
+++ markdown2dvp.hs
@@ -29,13 +29,20 @@ freely, subject to the following restrictions:
 
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE LambdaCase #-}
+
+-- App
+import Debug.Trace
+import           Data.Text.ICU.Convert as ICU
+import qualified Data.Text.IO as TIO
 
 -- Usefull
 import           Data.String (IsString(..))
 import           Data.Default (Default(..))
 import           Control.Monad.State
 import           Data.Maybe
-import           Data.Set
+import qualified Data.Set as Set
+import           Data.Char (ord)
 -- App
 import           System.IO
 -- Pandoc
@@ -44,6 +51,8 @@ import           Text.Pandoc.Shared (escapeStringUsing)
 -- UTF8
 import           Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import qualified Data.ByteString.Char8 as B
 -- XML
 import           Text.XML.Light (unode,
                                  Content(..),
@@ -54,6 +63,7 @@ import           Text.XML.Light (unode,
                                  useExtraWhiteSpace,
                                  prettyConfigPP)
 import qualified Text.XML.Light as XML
+
 
 -- Types
 type XML = [Content]
@@ -71,21 +81,33 @@ dvpToString (DVP s) = s
 
 type Notes = [[Block]]
 type Refs = [([Inline], Target)]
-data WriterState = WriterState { stNotes :: Notes
-                               , stRefs  :: Refs
-                               , stIds   :: [String]
-                               , stPlain :: Bool
-                               , stLatex :: Int
-                               , stLogs  :: [String]}
+type SectionHeader = (Int, String, XML)
+data WriterState = WriterState { stNotes        :: Notes
+                               , stRefs         :: Refs -- do we need it ??
+                               , stLatex        :: Int
+                               , stPlain        :: Bool
+                               , stLogs         :: [String]}
 instance Default WriterState
-  where def = WriterState{ stNotes = [], stRefs = [], stIds = [],
-                           stPlain = False, stLatex = 0, stLogs = [] }
+  where def = WriterState { stNotes = []
+                          , stRefs = []
+                          , stLatex = 0
+                          , stLogs = []
+                          , stPlain = False
+                          }
 incrementLatex :: State WriterState ()
 incrementLatex = modify (\s -> s {stLatex = 1 + stLatex s})
 
 (<>) :: Node t => String -> t -> Element
 (<>) = unode
 infixr 8 <>
+
+(<^>) :: Node t => String -> t -> Content
+(<^>) =  fmap (fmap Elem) unode
+infixr 8 <^>
+
+(<!>) :: Node t => String -> t -> XML
+(<!>) =  fmap (fmap toXML) unode
+infixr 8 <!>
 
 (|.) :: [XML.Attr] -> t -> ([XML.Attr], t)
 (|.) args content = (args, content)
@@ -151,8 +173,52 @@ warp w is f = do
   content <- inlineListToXML w is
   return . toXML $ f content
 
-blockToXML :: WriterOptions -> Block -> State WriterState XML
-blockToXML w bs = return emptyXML
+blockListToXML :: WriterOptions -> [Block] -> State WriterState XML
+blockListToXML w xs = do
+  blocks <- mapM (blockToXML w) xs
+  sectionify blocks
+  where
+    -- A well structured document doesnt contain stuff outside a section.
+    -- But not all documents are well structured :) Anyway, the dvp kit is
+    -- permissive enougth to build even if the XML isn't valid.
+    sectionify :: [Either XML SectionHeader] -> State WriterState XML
+    sectionify [] = return []
+    sectionify ((Right (level, id, title)) : xs) = do
+      let (subtree, neighboor) = span (belongToSubtree level) xs
+      xmlSubtree <- sectionify subtree
+      xmlNeighboor <- sectionify neighboor
+      return $ (:)
+        ("section" <^> ["id" |= id] |. (("title" <^> title) : xmlSubtree))
+        (xmlNeighboor)
+    sectionify ((Left x) : xs) = (x++) `fmap` sectionify xs
+
+    belongToSubtree :: Int -> Either XML SectionHeader -> Bool
+    belongToSubtree _ (Left _) = True
+    belongToSubtree n (Right (level, _, _)) = level > n
+
+
+paragraph :: XML -> Either XML SectionHeader
+paragraph = Left . toXML . ("paragraph" <>)
+
+htmlBrut :: XML -> Either XML SectionHeader
+htmlBrut = Left . toXML . ("html-brut" <>)
+
+blockToXML :: WriterOptions -> Block -> State WriterState (Either XML SectionHeader)
+blockToXML w  (Para is) = paragraph `liftM` warp w is id
+blockToXML w (Para [Image alt (src, 'f':'i':'g':':':title)]) =
+  blockToXML w (Para [Image alt (src, title)])
+blockToXML w  (Para [Image is (url, title)]) = do
+  content <- inlineListToXML w is
+  let alt = concat $ fmap showContent content
+  return . Left $ "image" <!> ["src" |= url, "titre" |= title, "alt" |= alt] |. emptyXML
+blockToXML w  (Plain is) = liftM htmlBrut $
+  withState (\s -> s {stPlain = True}) $ warp w is id
+blockToXML w  (Plain is) = liftM htmlBrut $
+  withState (\s -> s {stPlain = True}) $ warp w is id
+blockToXML w (Header level (id, _, _) is) = do
+  title <- inlineListToXML w is
+  return $ Right (level, id, title)
+blockToXML w bs = return . Left $ "BLOCK" <!> "UNKNOWNN"
 
 inlineToXML w (Emph is) = warp w is $ \content ->
   "i" <> content
@@ -170,15 +236,14 @@ inlineToXML w (SmallCaps is) = warp w is $ \content ->
 inlineToXML w (Quoted _ is) =  warp w is $ \content ->
   concat [rawText "&#171;&#160;", content, rawText "&#160;&#187;"]
 -- Dvp inline code isn't allowed. We use <inline>, without coloration.
-inlineToXML _ (Code _ str) = return . toXML $ "inline" <> str
-inlineToXML _ (Str str) = return $ toXML str
+inlineToXML _ (Code _ str) = return $ "inline" <!> str
+inlineToXML _ (Str str) = return . toXML $ str
 inlineToXML _ (Space) = return $ toXML " "
 -- Inline latex
 inlineToXML w (Math InlineMath str) = do
-  modify (\s -> s { stLogs = ["plouf"] })
   id <- fmap (("latex-" ++) . show) $ gets stLatex
   incrementLatex
-  return . toXML $ "latex" <> ["id" |= id] |. (verbaText str)
+  return $ "latex" <!> ["id" |= id] |. (verbaText str)
 -- Raw stuff isn't supported
 inlineToXML w (RawInline f str) = do
   modify (\s -> s { stLogs = msg : stLogs s })
@@ -190,20 +255,24 @@ inlineToXML w c@(Cite _ is) = do
   warp w is id
   where
     msg = "Citation not supported : " ++ (show c)
-inlineToXML w (Link is (url, "")) = warp w is $ \content ->
-  "link" <> ["href" |= url] |. content
-inlineToXML w (Link is (url, title)) = warp w is $ \content ->
-  "link" <> ["href" |= url, "title" |= title] |. content
+inlineToXML w (Link is (url, "")) = do
+  content <- inlineListToXML w is
+  tag <- (\case {True -> "a" ; False -> "link"}) `liftM` gets stPlain
+  return $ tag <!> ["href" |= url] |. content
+inlineToXML w (Link is (url, title)) = do
+  content <- inlineListToXML w is
+  tag <- (\case {True -> "a" ; False -> "link"}) `liftM` gets stPlain
+  return $ tag <!> ["href" |= url, "title" |= title] |. content
 inlineToXML w (Image is (url, title)) = warp w is $ \content ->
   let alt = concat $ fmap showContent content in
-  "image" <> ["href" |= url, "title" |= title, "alt" |= alt] |. emptyXML
+  "image" <!> ["src" |= url, "titre" |= title, "alt" |= alt] |. emptyXML
 inlineToXML w (Note bs) = case isEnabled Ext_footnotes w of
     True -> do
       modify (\st -> st{ stNotes = bs : stNotes st })
       ref <- (show . length) `fmap` gets stNotes
       return . concat . fmap toXML $ ["[", ref, "]"]
     False -> do
-      content <- fmap concat . mapM (blockToXML w) $ bs
+      content <- blockListToXML w $ bs
       return . concat $ [toXML "[", content , toXML "]"]
 
 inlineToXML _ x = return . toXML . show $ x
@@ -212,12 +281,13 @@ authorToXML :: WriterOptions -> [Inline] -> State WriterState XML
 authorToXML = inlineListToXML
 
 pandocToDvp :: WriterOptions -> Pandoc -> State WriterState DVP
-pandocToDvp opts (Pandoc (Meta title authors date) blocks) = do
-  title' <- fmap ("titre" <>) $ inlineListToXML opts title
-  page' <- fmap ("titre" <>) $ inlineListToXML opts title
-  authors' <- fmap concat . mapM (authorToXML opts) $ authors
-  date' <- inlineListToXML opts date
-  headerblock <- return . Elem $ "entete" <>
+pandocToDvp w (Pandoc (Meta title authors date) blocks) = do
+  trace (show blocks) $ return ()
+  title' <- inlineListToXML w title
+  page' <- inlineListToXML w title
+  authors' <- fmap concat . mapM (authorToXML w) $ authors
+  date' <- inlineListToXML w date
+  headerblock <- return $ "entete" <!>
       [ "rubrique"  <> "89"
       , "meta"      <> ["description" <> "", "keywords" <> ""]
       , "titre"     <> ["page" <> page', "article" <> title']
@@ -228,13 +298,30 @@ pandocToDvp opts (Pandoc (Meta title authors date) blocks) = do
       , "lictype"   <> "6"
       , "licannee"  <> "2013"
       , "serveur"   <> "zenol-http"
-      , "chemin"    <> "relative/path/"
+      , "chemin"    <> "relative/path"
       , "urlhttp"   <> "http://cochoy-jeremy.developpez.com/relative/path/"
       , "pdf"       <> ["sautDePageAvantSection" <> "0",
                         "notesBasPage" <> "FinDocument"]
       ]
-  authorsblock <- return . Elem $ "authorDescriptions" <> authors'
-  return $ renderDvp [headerblock, authorsblock]
+  authorsblock <- return $ "authorDescriptions" <!> authors'
+  content <- blockListToXML w blocks
+  foot <- case isEnabled Ext_footnotes w of
+    False -> return $ emptyXML
+    True  -> fmap (paragify . listify) $ xmlify =<< gets stNotes
+  return . renderDvp . concat $ [ headerblock
+                                , authorsblock
+                                , "summary" <!> (content ++ foot)
+                                ]
+  where
+    paragify :: XML -> XML
+    paragify xml = "section" <!> ["title" <> "Références", "paragraph" <> xml]
+    listify :: [XML] -> XML
+    listify xmls = "liste" <!> map (\s -> "element" <^> ["useText" |= "0"] |. s) xmls
+    xmlify :: [[Block]] -> State WriterState [XML]
+    xmlify = mapM (blockListToXML w)
+      where
+        clean (Left x) = return x
+        clean (Right _) = return emptyXML
 
 {- BEGINING OF THE SCRIPT -}
 
@@ -244,16 +331,21 @@ pandocToDvp opts (Pandoc (Meta title authors date) blocks) = do
 
 main :: IO ()
 main = do
-  s <- getContents
+  s <- T.unpack `liftM` TIO.getContents
   o <- return . writeDvp (def) . readMarkdown readerOpts $ s
   mapM_ (hPutStrLn stderr) $ snd o
-  putStrLn . dvpToString $ fst o
+  B.putStrLn . B.pack . escape . dvpToString $ fst o
+  where
+    escape :: String -> String
+    escape s = s >>= \c -> if c < '\x007f'
+                           then [c]
+                           else "&#" ++ show (ord c) ++ ";"
 
-writerNoFootnote s = s { writerExtensions = writerExtensions s \\ Data.Set.fromList [Ext_footnotes]}
+writerNoFootnote s = s { writerExtensions = writerExtensions s Set.\\ Set.fromList [Ext_footnotes]}
 
 readerOpts = def
     { readerSmart = True
-    , readerExtensions = unions [pandocExtensions, multimarkdownExtensions]
+    , readerExtensions = Set.unions [pandocExtensions, multimarkdownExtensions]
     }
 
 {- END OF THE SCRIPT -}
